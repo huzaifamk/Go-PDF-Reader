16f7ab2bb630b6ef7f5ec4a8d652ef09d4de65a3
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-07-29 16:08:44 +0500

    Fix v2

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,55 @@
 package utils
 
-import "github.com/ledongthuc/pdf"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/ledongthuc/pdf"
+)
+
+func ReadPdf(path string) (string, error) {
+	f, r, err := pdf.Open(path)
+	// remember close file
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	var buf bytes.Buffer
+	b, err := r.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+	buf.ReadFrom(b)
+	return buf.String(), nil
+}
+
+func ReadPdf2(path string) (string, error) {
+	f, r, err := pdf.Open(path)
+	// remember close file
+	defer f.Close()
+	if err != nil {
+		return "", err
+	}
+	totalPage := r.NumPage()
+
+	for pageIndex := 1; pageIndex <= totalPage; pageIndex++ {
+		p := r.Page(pageIndex)
+		if p.V.IsNull() {
+			continue
+		}
+		var lastTextStyle pdf.Text
+		texts := p.Content().Text
+		for _, text := range texts {
+			if IsSameSentence(text, lastTextStyle) {
+				lastTextStyle.S = lastTextStyle.S + text.S
+			} else {
+				fmt.Printf("Font: %s, Font-size: %f, x: %f, y: %f, content: %s \n", lastTextStyle.Font, lastTextStyle.FontSize, lastTextStyle.X, lastTextStyle.Y, lastTextStyle.S)
+				lastTextStyle = text
+			}
+		}
+	}
+	return "", nil
+}
 
 func IsSameSentence(text pdf.Text, lastTextStyle pdf.Text) bool {
 	return (text.Font == lastTextStyle.Font) && (text.FontSize == lastTextStyle.FontSize) && (text.X == lastTextStyle.X)
