ed346081dc77f50cde518372b75d9179a6ce060e
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-08-01 14:57:41 +0500

    Fex fixes

--- main.go
+++ main.go
@@ -12,6 +12,11 @@ func main() {
 		panic(err)
 	}
 
+	// content, err := utils.ReadPdfTextWithFormatting("test_files/0478_s16_qp_11.pdf")
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 	os.WriteFile("output.txt", []byte(content), 0644)
 
 	utils.FormatLines("output.txt")
