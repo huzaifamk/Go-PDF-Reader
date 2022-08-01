282f53057c0f79236382191f123b37aa6b64dc52
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-08-01 15:44:15 +0500

    Improved formatting

--- utils/utils.go
+++ utils/utils.go
@@ -118,6 +118,57 @@ func FormatLines(filename string) {
 		if strings.Contains(line, `(a)`) {
 			lines[i] = strings.Replace(line, `(a)`, "\n\n(a)", -1)
 		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `(b)`) {
+			lines[i] = strings.Replace(line, `(b)`, "\n\n(b)", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `(c)`) {
+			lines[i] = strings.Replace(line, `(c)`, "\n\n(c)", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `(d)`) {
+			lines[i] = strings.Replace(line, `(d)`, "\n\n(d)", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `(f)`) {
+			lines[i] = strings.Replace(line, `(f)`, "\n\n(f)", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `[1]`) {
+			lines[i] = strings.Replace(line, `[1]`, "\n\n[1]", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `[2]`) {
+			lines[i] = strings.Replace(line, `[2]`, "\n\n[2]", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `[3]`) {
+			lines[i] = strings.Replace(line, `[3]`, "\n\n[3]", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `[4]`) {
+			lines[i] = strings.Replace(line, `[4]`, "\n\n[4]", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `[5]`) {
+			lines[i] = strings.Replace(line, `[5]`, "\n\n[5]", -1)
+		}
+	}
+	for i, line := range lines {
+		if strings.Contains(line, `[6]`) {
+			lines[i] = strings.Replace(line, `[6]`, "\n\n[6]", -1)
+		}
+	}
 
 		output := strings.Join(lines, "\n")
 		err = ioutil.WriteFile(filename, []byte(output), 0644)
@@ -125,4 +176,3 @@ func FormatLines(filename string) {
 			log.Fatalln(err)
 		}
 	}
-}
