105a4608f6542e03a5815f0e8b96c001068b51e5
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-08-02 09:34:10 +0500

    Edited main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"pdfreader/format"
 	utils "pdfreader/utils"
 )
 
@@ -12,17 +13,7 @@ func main() {
 		panic(err)
 	}
 
-	// content, err := utils.ReadPdfTextWithFormatting("test_files/0478_s16_qp_11.pdf")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// content, err := utils.ReadPdfRow("test_files/0478_s16_qp_11.pdf")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
 	os.WriteFile("output.txt", []byte(content), 0644)
 
-	utils.FormatLines("output.txt")
+	format.FormatLines("output.txt")
 }
