6882c885c4ae45c5731d60faf6961508fbd96713
HuzaifaMk <76698805+HuzaifaMk@users.noreply.github.com>
2022-08-04 13:52:03 +0500

    Added file path

--- main.go
+++ main.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"os"
-	"pdfreader/format"
+	// "pdfreader/format"
 	utils "pdfreader/utils"
 )
 
 func main() {
 
-	content, err := utils.ReadPdfText("test_files/0478_s16_qp_11.pdf")
+	content, err := utils.ReadPdfText("test_files/Test_Pdf.pdf")
 	if err != nil {
 		panic(err)
 	}
 
 	os.WriteFile("output.txt", []byte(content), 0644)
 
-	format.UpdateOutput("output.txt")
+	// format.UpdateOutput("output.txt")
 
-	format.FormatLines("output.txt")
+	// format.FormatLines("output.txt")
 }
